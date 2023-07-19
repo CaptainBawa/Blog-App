1556049ee45150e15be4ca0bb4892a5c800db247
Collins Bawa <collinskwamebawa@yahoo.com>
2023-07-19 17:57:23 +0000

    Generate schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_18_222800) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_19_175413) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_18_222800) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "users_id", null: false
+    t.bigint "posts_id", null: false
+    t.index ["posts_id"], name: "index_comments_on_posts_id"
     t.index ["users_id"], name: "index_comments_on_users_id"
   end
 
@@ -49,6 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_18_222800) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "posts", column: "posts_id"
   add_foreign_key "comments", "users", column: "users_id"
   add_foreign_key "likes", "users", column: "users_id"
   add_foreign_key "posts", "users", column: "author_id"
