92cb67683f24d898409e3f955e322b9f8bca910d
Collins Bawa <collinskwamebawa@yahoo.com>
2023-07-19 21:55:49 +0000

    Add comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,13 @@ class User < ApplicationRecord
     has_many :posts
     has_many :likes
 
+    # The function retrieves the three most recent posts written 
+    # by the author.
     def three_most_recent_posts
         posts = Post.where(author_id: self.id).order(created_at: :desc).limit(3)
     end
 end
 
 # Code to verify if the three_most_recent_posts method is working
-# user = User.find_by(name: "John Doe")
+# user = User.find_by(Name: "Kwame")
 # user.three_most_recent_posts
