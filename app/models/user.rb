3daabd3fceecfdae7d5ec0a7a23a0f3135ecdbd0
Collins Bawa <collinskwamebawa@yahoo.com>
2023-07-19 21:46:06 +0000

    Add method to get three recent post from a given user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ApplicationRecord
     has_many :comments
     has_many :posts
     has_many :likes
+
+    def three_most_recent_posts
+        posts = Post.where(author_id: self.id).order(created_at: :desc).limit(3)
+    end
 end
+
+# Code to verify if the three_most_recent_posts method is working
+# user = User.find_by(name: "John Doe")
+# user.three_most_recent_posts
