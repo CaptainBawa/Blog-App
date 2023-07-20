44252662589011c002e83f3cbe6cb1802b31b655
Collins Bawa <collinskwamebawa@yahoo.com>
2023-07-20 17:26:00 +0000

    Add validation to the name and post counter column

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,14 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: 'author_id', counter_cache: true
   has_many :likes
 
+  # The code `validates :Name, presence: true` is validating that the `Name`
+  # attribute of the `User` model is present, meaning it cannot be blank or nil.
+  validates :Name, presence: true
+  # is adding a validation to the `User` model. It ensures that the `PostCounter` attribute is a
+  # numerical value and that it is greater than or equal to 0. This means that the `PostCounter`
+  # attribute cannot be a non-integer value or a negative number.
+  validates :PostCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   # The function retrieves the three most recent posts written
   # by the author.
   def three_most_recent_posts
