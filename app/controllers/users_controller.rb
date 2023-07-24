2f2919655822dd6d72e4b4883a8f460279630486
Collins Bawa <collinskwamebawa@yahoo.com>
2023-07-24 21:57:26 +0000

    Fix linters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,12 @@
 class UsersController < ApplicationController
+  # The index function retrieves all users from the database.
+  def index
+    @users = User.all
+  end
 
-    # The index function retrieves all users from the database.
-    def index
-        @users = User.all
-    end
-
-    # The function retrieves a user object from the database
-    # based on the provided ID.
-    def show 
-        @user = User.find(params[:id])
-    end
+  # The function retrieves a user object from the database
+  # based on the provided ID.
+  def show
+    @user = User.find(params[:id])
+  end
 end
