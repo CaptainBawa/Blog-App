37567b538116994ce03dc78cb1afa2bc81206c8c
Collins Bawa <collinskwamebawa@yahoo.com>
2023-07-24 21:29:08 +0000

    Add index and show actin that lists all users and also list a single user

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+
+    # The index function retrieves all users from the database.
+    def index
+        @users = User.all
+    end
+
+    # The function retrieves a user object from the database
+    # based on the provided ID.
+    def show 
+        @user = User.find(params[:id])
+    end
+end
