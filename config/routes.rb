e792a291db6e7aec23ed66d727b7141ef02d28aa
Collins Bawa <collinskwamebawa@yahoo.com>
2023-07-24 20:45:18 +0000

    Add app routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "user_posts/users/:user_id", to: "posts#index"
+  get "/user_post/:id", to: "posts#show"
+  get "/users", to: "users#index"
+  get "/users/:id", to: "users#show"
 end
