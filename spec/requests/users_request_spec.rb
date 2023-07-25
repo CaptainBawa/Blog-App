f035e6356ad67ee2da8770ba82eb1aaed606becc
Collins Bawa <collinskwamebawa@yahoo.com>
2023-07-25 20:04:12 +0000

    Add test case for the show action in the users controller

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'UsersController', type: :request do
-    describe 'GET /users' do 
+    describe 'GET /users#index' do 
         it 'should check if respose status code is correct' do 
             get users_path
             expect(response).to have_http_status(200)
@@ -9,6 +9,17 @@ RSpec.describe 'UsersController', type: :request do
         it 'should check if the response body includes correct placeholder text' do 
             get users_path
             expect(response.body).to include('Here is a list of all users')
+        end
+    end
+    describe 'GET /user#show' do 
+        user = User.create(name: 'John Doe', photo: 'photo_url', bio: 'bio_text', post_counter: 0)
+        it 'should check if respose status code is correct' do 
+            get user_path(id: user.id)
+            expect(response).to have_http_status(200)
+        end
+        it 'should check if the response body includes correct placeholder text' do 
+            get user_path(id: user.id)
+            expect(response.body).to include('Here is a list of a specific user')
         end
     end
 end
