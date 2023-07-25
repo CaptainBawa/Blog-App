479d46ecb8a2b506100c1a017a86121469c13a31
Collins Bawa <collinskwamebawa@yahoo.com>
2023-07-25 20:45:58 +0000

    Fix linters

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,33 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe 'UsersController', type: :request do
-    describe 'GET /users#index' do 
-        it 'should check if respose status code is correct' do 
-            get users_path
-            expect(response).to have_http_status(200)
-        end
-        it 'should check if the response body includes correct placeholder text' do 
-            get users_path
-            expect(response.body).to include('Here is a list of all users')
-        end
-        it 'should render the index template' do
-            get users_path
-            expect(response).to render_template(:index)
-        end
+  describe 'GET /users#index' do
+    it 'should check if respose status code is correct' do
+      get users_path
+      expect(response).to have_http_status(200)
     end
-    describe 'GET /user#show' do 
-        user = User.create(name: 'John Doe', photo: 'photo_url', bio: 'bio_text', post_counter: 0)
-        it 'should check if respose status code is correct' do 
-            get user_path(id: user.id)
-            expect(response).to have_http_status(200)
-        end
-        it 'should check if the response body includes correct placeholder text' do 
-            get user_path(id: user.id)
-            expect(response.body).to include('Here is a list of a specific user')
-        end
-        it 'should render the show template' do
-            get user_path(id: user.id)
-            expect(response).to render_template(:show)
-        end
+    it 'should check if the response body includes correct placeholder text' do
+      get users_path
+      expect(response.body).to include('Here is a list of all users')
     end
+    it 'should render the index template' do
+      get users_path
+      expect(response).to render_template(:index)
+    end
+  end
+  describe 'GET /user#show' do
+    user = User.create(name: 'John Doe', photo: 'photo_url', bio: 'bio_text', post_counter: 0)
+    it 'should check if respose status code is correct' do
+      get user_path(id: user.id)
+      expect(response).to have_http_status(200)
+    end
+    it 'should check if the response body includes correct placeholder text' do
+      get user_path(id: user.id)
+      expect(response.body).to include('Here is a list of a specific user')
+    end
+    it 'should render the show template' do
+      get user_path(id: user.id)
+      expect(response).to render_template(:show)
+    end
+  end
 end
