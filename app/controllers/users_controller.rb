6381a260619e90bc8d7de7b7b0f0fe71639e46ff
Collins Bawa <collinskwamebawa@yahoo.com>
2023-07-27 16:32:40 +0000

    Add method to get first user from the database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @posts = @user.posts
   end
+
+  # The current_user function returns the first user object and assigns it
+  # to the instance variable @current_user if it is not already assigned.
+  def current_user
+    @current_user ||= User.first 
+  end
 end
