f68ab95bdfcbee33ca805f70045c0c79a13bc779
Isabella <isabellaotoo25@gmail.com>
2023-07-31 13:08:20 +0000

    Solve n+1 problem

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   # The index function retrieves a user and their posts, and initializes a new post object.
   def index
     @user = User.find(params[:author_id])
-    @posts = @user.posts
+    @posts = @user.posts.includes(:comments)
     @post = Post.new
   end
 
