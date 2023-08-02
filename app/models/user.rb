4ac1887dd76ec7a00940295d6ba5a1e241bb6d71
Collins Bawa <collinskwamebawa@yahoo.com>
2023-08-02 18:11:04 +0000

    Add comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
+  # The line `devise :database_authenticatable, :registerable, :recoverable, :rememberable,
+  # :validatable, :confirmable` is configuring the Devise gem for the User model.
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
+
   has_many :comments, foreign_key: 'users_id'
   # counter_cache makes sure Rails automatically update the PostCounter
   # attribute in the User model when posts are created or destroyed.
