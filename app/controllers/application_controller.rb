56c5ee7e1625e33cfa0e17c5497bfddcca4d1409
Collins Bawa <collinskwamebawa@yahoo.com>
2023-08-02 17:53:49 +0000

    Uncomment forgery protection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
- # protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
   # The current_user function returns the first user object and assigns it
   # to the instance variable @current_user if it is not already assigned.
   # def current_user
