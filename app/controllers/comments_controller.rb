e9b562e3d54a990c674b549dc7018ce568d6b5ed
Collins Bawa <collinskwamebawa@yahoo.com>
2023-08-04 15:28:00 +0000

    Add destroy method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,6 +12,16 @@ class CommentsController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:users_id])
+    @post = Post.find(params[:posts_id])
+    @comment = Comment.find(params[:id])
+    authorize! :destroy, @comment
+    @comment.destroy
+    @comment.update_post_comments_counter
+    redirect_to user_post_path(@user, @post), notice: 'Comment deleted successfully.'
+  end
+
   private
 
   # The function `comment_params` is used to extract and permit the `text` parameter
